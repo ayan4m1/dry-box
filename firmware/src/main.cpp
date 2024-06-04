c9a68aa39330c70a5987556333a3ea5b9e0bd074
ayan4m1 <andrew@bulletlogic.com>
2024-06-04 00:52:10 -0400

    clean up strings and logic

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,21 +3,21 @@
 #include <FastLED.h>
 #include <WEMOS_SHT3X.h>
 
-#define DEVICE_NAME "Alpha"
+#define DEVICE_ID "Bin 1"
 #define WIFI_SSID "qux"
 #define WIFI_PSK "wi9NNYara"
 #define DISCORD_WEBHOOK_URL                               \
   "https://discord.com/api/webhooks/1247389183543218239/" \
   "Duwl0KRroGvqmGav2_KbKhcQP18uYCo80jQtrsyFXe8s7OQ9TGBlGDrCdCjrGUqQ7gbK"
 
-#define MESSAGE_BUFFER 64
-#define WARNING_THRESHOLD 40
-#define ALARM_THRESHOLD 60
+#define DISCORD_WARNING_ICON "warning"
+#define DISCORD_ALERT_ICON "bangbang"
+#define DISCORD_ERROR_ICON "no_entry_sign"
+#define DISCORD_CLEAR_ICON "white_check_mark"
+#define DISCORD_MSG_BUFFER 64
+#define HUM_WARN_THRESH 40
+#define HUM_ALARM_THRESH 60
 #define POLLING_RATE_MS 60000
-#define WARNING_ICON F("warning")
-#define ALARM_ICON F("bangbang")
-#define CLEAR_ICON F("white_check_mark")
-#define ERROR_ICON F("no_entry_sign")
 
 bool alarmTripped;
 Discord_Webhook webhook;
@@ -29,14 +29,15 @@ void setup() {
   webhook.connectWiFi();
 }
 
-void sendMessage(const __FlashStringHelper* icon, const float humidity) {
-  char message[MESSAGE_BUFFER];
+void sendMessage(const float humidity, const char *icon) {
+  char message[DISCORD_MSG_BUFFER];
 
   if (humidity > 0) {
-    sprintf(message, ":%s: %s is at %.2f%% humidity", icon, DEVICE_NAME,
+    sprintf(message, ":%s: %s is at %.2f%% humidity", icon, DEVICE_ID,
             humidity);
   } else {
-    sprintf(message, ":%s: %s is at unknown humidity!", icon, DEVICE_NAME);
+    sprintf(message, ":%s: %s has a sensor fault!", DISCORD_ERROR_ICON,
+            DEVICE_ID);
   }
 
   webhook.send(message);
@@ -46,20 +47,24 @@ bool updateSensor() { return sensor.get() == 0; }
 
 void loop() {
   EVERY_N_MILLIS(POLLING_RATE_MS) {
-    if (!updateSensor()) {
-      sendMessage(ERROR_ICON, -1);
+    if (alarmTripped) {
+      return;
     }
 
-    if (sensor.humidity > ALARM_THRESHOLD) {
+    if (!updateSensor()) {
+      sendMessage(-1, nullptr);
+    }
+
+    if (sensor.humidity > HUM_ALARM_THRESH) {
       if (!alarmTripped) {
         alarmTripped = true;
       }
-      sendMessage(ALARM_ICON, sensor.humidity);
-    } else if (sensor.humidity > WARNING_THRESHOLD) {
-      sendMessage(WARNING_ICON, sensor.humidity);
+      sendMessage(sensor.humidity, DISCORD_ALERT_ICON);
+    } else if (sensor.humidity > HUM_WARN_THRESH) {
+      sendMessage(sensor.humidity, DISCORD_WARNING_ICON);
     } else if (alarmTripped) {
       alarmTripped = false;
-      sendMessage(CLEAR_ICON, sensor.humidity);
+      sendMessage(sensor.humidity, DISCORD_CLEAR_ICON);
     }
   }
 }
