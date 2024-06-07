dd734c56fb34aff5a82d26caa68112f8f4038163
ayan4m1 <andrew@bulletlogic.com>
2024-06-07 12:01:23 -0400

    switch to WiFiManager based setup

--- firmware/flash-helper.py
+++ firmware/flash-helper.py
@@ -1,23 +1,17 @@
-import os
+from os import getenv
 
 Import('env')
 
 if env.IsIntegrationDump():
     Return()
 
-device_id = os.getenv('DEVICE_ID')
-wifi_ssid = os.getenv('WIFI_SSID')
-wifi_psk = os.getenv('WIFI_PSK')
-discord_webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
+device_id = getenv('DEVICE_ID')
 
-if device_id is None or wifi_ssid is None or wifi_psk is None or discord_webhook_url is None:
+if device_id is None:
     Return()
 
 env.Append(
     CPPDEFINES=[
-        ('DEVICE_ID', env.StringifyMacro(device_id)),
-        ('WIFI_SSID', env.StringifyMacro(wifi_ssid)),
-        ('WIFI_PSK', env.StringifyMacro(wifi_psk)),
-        ('DISCORD_WEBHOOK_URL', env.StringifyMacro(discord_webhook_url))
+        ('DEVICE_ID', env.StringifyMacro(device_id))
     ]
 )
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,53 +1,62 @@
-#include <Arduino.h>
-#include <Discord_WebHook.h>
-#include <ESP8266mDNS.h>
-#include <FastLED.h>
-#include <WEMOS_SHT3X.h>
-
 #ifndef DEVICE_ID
 #define DEVICE_ID ""
 #pragma warning "DEVICE_ID is required!"
 #endif
-#ifndef WIFI_SSID
-#define WIFI_SSID ""
-#pragma warning "WIFI_SSID is required!"
-#endif
-#ifndef WIFI_PSK
-#define WIFI_PSK ""
-#pragma warning "WIFI_PSK is required!"
-#endif
-#ifndef DISCORD_WEBHOOK_URL
-#define DISCORD_WEBHOOK_URL ""
-#pragma warning "DISCORD_WEBHOOK_URL is required!"
-#endif
+
+#define USE_LITTLEFS true
+#define DOUBLERESETDETECTOR_DEBUG false
+#define USE_DYNAMIC_PARAMETERS true
+#define USING_BOARD_NAME false
+
+#define MAX_PERCENTAGE_LEN 3
+#define MAX_POLLING_RATE_LEN 5
+#define MAX_DISCORD_WEBHOOK_LEN 192
+
+char sensorPollingRate[MAX_POLLING_RATE_LEN + 1] = "120";
+char discordWebhookUrl[MAX_DISCORD_WEBHOOK_LEN + 1] = "";
+char humidityAlarmThreshold[MAX_PERCENTAGE_LEN + 1] = "60";
+char humidityWarningThreshold[MAX_PERCENTAGE_LEN + 1] = "40";
+
+MenuItem menuItems[] = {
+    {"hwt", "Warn %", humidityWarningThreshold, MAX_PERCENTAGE_LEN},
+    {"hat", "Alarm %", humidityAlarmThreshold, MAX_PERCENTAGE_LEN},
+    {"dwu", "Webhook URL", discordWebhookUrl, MAX_DISCORD_WEBHOOK_LEN},
+    {"spr", "Polling Rate", sensorPollingRate, MAX_POLLING_RATE_LEN}};
+
+uint16_t NUM_MENU_ITEMS = sizeof(menuItems) / sizeof(MenuItem);
+
+#include <Arduino.h>
+#include <Discord_WebHook.h>
+#include <ESP8266mDNS.h>
+#include <ESP_WiFiManager_Lite.h>
+#include <FastLED.h>
+#include <WEMOS_SHT3X.h>
 
 #define DISCORD_WARNING_ICON "warning"
 #define DISCORD_ALERT_ICON "bangbang"
 #define DISCORD_ERROR_ICON "no_entry_sign"
 #define DISCORD_CLEAR_ICON "white_check_mark"
 #define DISCORD_MSG_BUFFER 64
-#define HUM_WARN_THRESH 40
-#define HUM_ALARM_THRESH 60
-#define POLLING_RATE_MS 60000
 
-bool alarmTripped;
-Discord_Webhook webhook;
 SHT3X sensor;
+bool alarmTripped;
+Discord_Webhook* webhook = new Discord_Webhook();
+ESP_WiFiManager_Lite* manager = new ESP_WiFiManager_Lite();
 
 String camelToSnake(String input) {
   String result = "";
 
-  char c = tolower(input[0]);
+  auto c = tolower(input[0]);
   result += char(c);
 
   for (int i = 1; i < input.length(); i++) {
     c = input[i];
 
     if (isupper(c)) {
-      result = result + '_';
+      result += '_';
       result += char(tolower(c));
     } else {
-      result = result + c;
+      result += c;
     }
   }
 
@@ -55,15 +64,13 @@ String camelToSnake(String input) {
 }
 
 void setup() {
-  webhook.begin(DISCORD_WEBHOOK_URL);
-  webhook.addWiFi(WIFI_SSID, WIFI_PSK);
-  webhook.connectWiFi();
-
-  MDNS.begin(camelToSnake(DEVICE_ID));
-  MDNS.addService("http", "tcp", 80);
+  manager->begin(camelToSnake(DEVICE_ID).c_str());
+  if (strlen(discordWebhookUrl) > 0) {
+    webhook->begin(discordWebhookUrl);
+  }
 }
 
-void sendMessage(const float humidity, const char *icon) {
+void sendMessage(const float humidity, const char* icon) {
   char message[DISCORD_MSG_BUFFER];
 
   if (humidity > 0) {
@@ -74,25 +81,29 @@ void sendMessage(const float humidity, const char *icon) {
             DEVICE_ID);
   }
 
-  webhook.send(message);
+  if (strlen(discordWebhookUrl) > 0) {
+    webhook->send(message);
+  }
 }
 
-bool updateSensor() { return sensor.get() == 0; }
-
 void loop() {
-  EVERY_N_MILLIS(POLLING_RATE_MS) {
+  uint16_t pollingRate = atoi(sensorPollingRate);
+  uint8_t alarmThreshold = atoi(humidityAlarmThreshold);
+  uint8_t warningThreshold = atoi(humidityWarningThreshold);
+
+  EVERY_N_MILLIS(pollingRate * 1e3) {
     if (alarmTripped) {
       return;
     }
 
-    if (!updateSensor()) {
+    if (sensor.get() == 0) {
       sendMessage(-1, nullptr);
     }
 
-    if (sensor.humidity > HUM_ALARM_THRESH) {
+    if (sensor.humidity > alarmThreshold) {
       alarmTripped = true;
       sendMessage(sensor.humidity, DISCORD_ALERT_ICON);
-    } else if (sensor.humidity > HUM_WARN_THRESH) {
+    } else if (sensor.humidity > warningThreshold) {
       sendMessage(sensor.humidity, DISCORD_WARNING_ICON);
     } else {
       alarmTripped = false;
