14fb390c75104ca8795b44184fba40e4fe759350
ayan4m1 <andrew@bulletlogic.com>
2024-06-09 01:57:34 -0400

    improve logic in flashing scripts

--- firmware/flash-helper.py
+++ firmware/flash-helper.py
@@ -2,14 +2,14 @@ from os import getenv
 
 Import('env')
 
-if env.IsIntegrationDump():
-    Return()
+device_id = ''
 
-device_id = getenv('DEVICE_ID')
+if not env.IsIntegrationDump():
+    device_id = getenv('DEVICE_ID')
 
-if device_id is None:
-    print('What is the device ID?')
-    device_id = input()
+    if device_id is None:
+        print('What is the device ID?')
+        device_id = input()
 
 env.Append(
     CPPDEFINES=[
--- firmware/flash.sh
+++ firmware/flash.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-is_num='^[0-9]+$'
-
 read -p "How many devices do you want to flash? " DEVICE_COUNT
 
-if ! [[ $DEVICE_COUNT =~ $is_num ]]; then
-    echo "Invalid device count!"
-    exit 1
-fi
-
-if [[ $DEVICE_COUNT -lt 1 ]]; then
+if [[ ! $DEVICE_COUNT =~ '^[0-9]+$' || $DEVICE_COUNT -lt 1 ]]; then
     echo "Invalid device count!"
     exit 1
 fi
@@ -22,9 +15,7 @@ else
     read -p "What is the device ID prefix? " DEVICE_ID_PREFIX
 
     for i in $(seq 1 $DEVICE_COUNT); do
-        echo "Flashing device ${i}"
-
+        read -p "Press ENTER when device ${i} is connected..."
         DEVICE_ID="${DEVICE_ID_PREFIX} ${i}" pio run -t upload
-        read -p "Press enter when next device is connected..."
     done
 fi
