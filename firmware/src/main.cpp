fab4531c2337c03f4b2e204d313c566e9c1953d7
ayan4m1 <andrew@bulletlogic.com>
2024-06-09 02:08:23 -0400

    remove flash helper scripts and make everything, including device name, configurable at runtime

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,8 +2,8 @@
 #define DOUBLERESETDETECTOR_DEBUG false
 #define USE_DYNAMIC_PARAMETERS true
 #define USING_BOARD_NAME false
-// set to 0 to disable WiFiManager logging
-#define _ESP_WM_LITE_LOGLEVEL_ 3
+// set to 3 or 4 to enable info/debug logging respectively
+#define _ESP_WM_LITE_LOGLEVEL_ 0
 #define REQUIRE_ONE_SET_SSID_PW true
 #define USING_MRD false
 
@@ -14,12 +14,7 @@
 #include <FastLED.h>
 #include <WEMOS_SHT3X.h>
 
-#ifndef DEVICE_ID
-#define DEVICE_ID ""
-#pragma warning "DEVICE_ID is required!"
-#endif
-
-#define WIFI_PSK "humidity"
+#define WIFI_PSK "drybox123"
 
 #define DISCORD_WARNING_ICON "warning"
 #define DISCORD_ALERT_ICON "bangbang"
@@ -28,6 +23,7 @@
 #define DISCORD_MSG_BUFFER 64
 
 // start dynamic params
+#define DEFAULT_DEVICE_NAME "Dry Box 1"
 #define DEFAULT_POLLING_RATE "120"
 #define DEFAULT_WEBHOOK_URL ""
 #define DEFAULT_ALARM_THRESHOLD "30"
@@ -35,11 +31,13 @@
 
 #define MAX_PERCENTAGE_LEN 3
 #define MAX_POLLING_RATE_LEN 5
+#define MAX_DEVICE_NAME_LEN 128
 #define MAX_DISCORD_WEBHOOK_LEN 192
 
 bool LOAD_DEFAULT_CONFIG_DATA = false;
 ESP_WM_LITE_Configuration defaultConfig;
 
+char deviceName[MAX_DEVICE_NAME_LEN + 1] = DEFAULT_DEVICE_NAME;
 char sensorPollingRate[MAX_POLLING_RATE_LEN + 1] = DEFAULT_POLLING_RATE;
 char discordWebhookUrl[MAX_DISCORD_WEBHOOK_LEN + 1] = DEFAULT_WEBHOOK_URL;
 char humidityAlarmThreshold[MAX_PERCENTAGE_LEN + 1] = DEFAULT_ALARM_THRESHOLD;
@@ -47,7 +45,8 @@ char humidityWarningThreshold[MAX_PERCENTAGE_LEN + 1] =
     DEFAULT_WARNING_THRESHOLD;
 
 MenuItem myMenuItems[] = {
-    {"hwt", "Warn %", humidityWarningThreshold, MAX_PERCENTAGE_LEN},
+    {"snn", "Device Name", deviceName, MAX_DEVICE_NAME_LEN},
+    {"hwt", "Warning %", humidityWarningThreshold, MAX_PERCENTAGE_LEN},
     {"hat", "Alarm %", humidityAlarmThreshold, MAX_PERCENTAGE_LEN},
     {"dwu", "Discord Webhook", discordWebhookUrl, MAX_DISCORD_WEBHOOK_LEN},
     {"spr", "Poll Rate (s)", sensorPollingRate, MAX_POLLING_RATE_LEN}};
@@ -75,12 +74,10 @@ String camelToSnake(const char* src) {
     if (isupper(c)) {
       result += '_';
       result += char(tolower(c));
-    } else {
+    } else if (isAlphaNumeric(c)) {
       result += c;
     }
   }
-
-  result.replace(" ", "");
 
   return result;
 }
@@ -89,11 +86,11 @@ void sendMessage(const float humidity, const char* icon) {
   char message[DISCORD_MSG_BUFFER];
 
   if (humidity > 0) {
-    sprintf(message, ":%s: %s is at %.2f%% humidity", icon, DEVICE_ID,
+    sprintf(message, ":%s: %s is at %.2f%% humidity", icon, deviceName,
             humidity);
   } else {
     sprintf(message, ":%s: %s has a sensor fault!", DISCORD_ERROR_ICON,
-            DEVICE_ID);
+            deviceName);
   }
 
   if (strlen(discordWebhookUrl) > 0) {
@@ -110,14 +107,13 @@ void pollSensor() {
     return;
   }
 
-  Serial.println(F("Polling sensor..."));
-
   if (sensor.get() != 0) {
+    Serial.println(F("Error polling sensor!"));
     sendMessage(-1, nullptr);
     return;
   }
 
-  Serial.printf("%.2f%% humidity\n", sensor.humidity);
+  Serial.printf("Humidity is %.2f%%\n", sensor.humidity);
 
   if (sensor.humidity > alarmThreshold) {
     alarmTripped = true;
@@ -136,8 +132,10 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
 
-  auto hostname = camelToSnake(DEVICE_ID);
-  Serial.printf("Startup using hostname %s\n", hostname.c_str());
+  // load the dynamic data we need to get the device name
+  manager->extLoadDynamicData();
+  auto hostname = camelToSnake(deviceName);
+  Serial.printf("Device name is %s\n", hostname.c_str());
 
   manager->setConfigPortal(hostname, WIFI_PSK);
   manager->begin(hostname.c_str());
